4ed5e3d60d16ca3cf390d5b4a400b4949687ee77
Javilonte <javgofi@gmail.com>
2026-05-27 05:09:48 -0600

    sd

---
+++ A_Game_with_Integers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){1
+    
+    int n, c; 
+    cin >> n;
+
+    int turn = 10;
+    for(int i; i=10; i++){
+        if(n + 1 == % 3 == 0)
+        cout << "First" << endl;
+        return 0;
+    }
+
+    if(n % 3 == 0){
+        c++;
+        
+    }
+
+    if (c == 10){
+        cout << "Second" << endl;
+    }
+
+    cout << "YES" << endl;
+    
+    return 0;
+}
